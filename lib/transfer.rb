1ebe7e192f2ad7298970d6b2bcd93278a2178ff7
Travis T <travistokuyama@gmail.com>
2018-09-26 19:40:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.status = "complete"  
+    if self.status == "complete"  
       sender.balance += amount
       receiver.balance -= amount
       self.status = "reversed"
